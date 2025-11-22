32ae8a2ffac6138ef1c40e1ef5b0f1cd9337fa9c
Yaroslav Kovalchuk <yaroslavkovalc03@gmail.com>
2025-11-22 14:56:09 +0200

    first upload

---
+++ js/mobile-menu.js
@@ -0,0 +1,10 @@
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuBtnOpen = document.querySelector('.menu-btn-open');
+const menuBtnClose = document.querySelector('.menu-btn-close');
+
+const toggleMenu = () => {
+  mobileMenu.classList.toggle('is-open');
+};
+
+menuBtnOpen.addEventListener('click', toggleMenu);
+menuBtnClose.addEventListener('click', toggleMenu);
